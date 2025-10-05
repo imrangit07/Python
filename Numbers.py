32f5dacd34239d5e2f7c78ba639ece41088301f6
imrangit07 <imranhussain21309@gmail.com>
2025-10-05 12:29:14 +0530

    Add number

---
+++ Numbers.py
@@ -0,0 +1,70 @@
+# # num1 = 90
+# # print(num1)
+
+# x = int(2.5)
+# y = 3
+# z = 4
+
+# #  Addition
+# print(x+y+z)
+
+# #  Subtraction (or Difference)
+# print(z-y)
+
+# # power
+# print(x*y)
+
+# # power
+# print(x**y)
+
+
+# print(y % 2)
+
+
+# result = 1/2.0
+# print(int(result))
+
+
+# # read about this in python documentation
+# repr('imran')
+
+# str('imran')
+
+# print('imran')
+
+# print(1 < 3)
+# print(1 == 3)
+# print(1 != 3 and 1 != 2)
+
+
+# import math
+# import random
+# print (math.floor(4.5))
+# print (math.floor(-4.5))
+# print (math.trunc(4.6))
+# print (math.ceil(4.6))
+# print (random.random())
+
+
+# imgn=2 + 1j
+# print (imgn * 5)
+
+
+# # octal = 0o10
+# print(0o20)
+
+# # hexadecimal = 0x10
+# print(0xFF)
+
+# # binary = 0b10101
+# print(0b1000)
+
+
+# print(oct(64))
+# print(hex(64))
+# print(bin(64))
+
+
+# print(int("0o100",8))
+# print(int("0x40",16))
+# print(int("0b1000000",2))
