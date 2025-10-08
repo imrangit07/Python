542cc55d12508cd6fd869d206c854208e79d2e6b
imrangit07 <imranhussain21309@gmail.com>
2025-10-08 21:06:52 +0530

    Add Condition file

---
+++ Condition/AgeGroup.py
@@ -0,0 +1,13 @@
+
+
+age=int(input("Give your age: "))
+
+if(age<=13):
+    print("Your are child")
+elif(age>13 and age<=18):
+    print("You are teenager")
+elif(age>18 and age<65):
+    print("You are adult")
+else:
+    print("You are senior")
+
